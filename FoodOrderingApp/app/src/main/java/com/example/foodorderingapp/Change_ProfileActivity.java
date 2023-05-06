23347015b4a4f602c34ef4704df19fde7224ed63
21520478 <21520478@gm.uit.edu.vn>
2023-05-06 20:04:23 +0700

    UI 2

---
+++ FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/Change_ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodorderingapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Change_ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile);
+    }
+}
